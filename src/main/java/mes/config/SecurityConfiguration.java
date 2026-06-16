e30c12d117ca2c2f2e173f6482e009e61734b8d7
LeeJaeWook <ljw960214@naver.com>
2026-06-16 17:36:57 +0900

    20260616_이재욱_파일다운로드 permitall 추가

--- src/main/java/mes/config/SecurityConfiguration.java
+++ src/main/java/mes/config/SecurityConfiguration.java
@@ -88,7 +88,8 @@ public class SecurityConfiguration {
                                 // API 및 외부 연동
                                 "/useridchk/**", "/user-auth/**", "/user-auth/save",
                                 "/popbill/webhook", "/api/transaction/input/**",
-                                "/api/das_device", "/authentication/**", "/api/common/**"
+                                "/api/das_device", "/authentication/**", "/api/common/**",
+                                "/api/files/download"
                         ).permitAll()
                         .antMatchers("/setup").hasAuthority("admin")
                         .anyRequest().authenticated()
